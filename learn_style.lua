730ae4987546e08503bd835565432e45cc18efa8
Evarin <champrobin@hotmail.com>
2015-12-28 14:17:45 +0100

    Full draft of global style learning

--- learn_style.lua
+++ learn_style.lua
@@ -17,9 +17,7 @@ cmd:option('-image_size', 256, 'Maximum height / width of generated image')
 cmd:option('-gpu', 0, 'Zero-indexed ID of the GPU to use; for CPU mode set -gpu = -1')
 
 -- Output options
-cmd:option('-print_iter', 50)
-cmd:option('-save_iter', 100)
-cmd:option('-output_image', 'out.png')
+cmd:option('-output_file', 'style.data')
 
 -- Other options
 cmd:option('-style_scale', 1.0)
@@ -32,116 +30,138 @@ cmd:option('-seed', -1)
 cmd:option('-style_layers', 'relu1_1,relu2_1,relu3_1,relu4_1,relu5_1', 'layers for style')
 
 function nn.SpatialConvolutionMM:accGradParameters()
-  -- nop.  not needed by our net
+   -- nop.  not needed by our net
 end
 
 local function main(params)
-  if params.gpu >= 0 then
-    require 'cutorch'
-    require 'cunn'
-    cutorch.setDevice(params.gpu + 1)
-  else
-    params.backend = 'nn-cpu'
-  end
+   if params.gpu >= 0 then
+      require 'cutorch'
+      require 'cunn'
+      cutorch.setDevice(params.gpu + 1)
+   else
+      params.backend = 'nn-cpu'
+   end
 
-  if params.backend == 'cudnn' then
-    require 'cudnn'
-    cudnn.SpatialConvolution.accGradParameters = nn.SpatialConvolutionMM.accGradParameters -- ie: nop
-  end
-  
-  print('loadcaffe')
-  local cnn = loadcaffe_wrap.load(params.proto_file, params.model_file, params.backend):float()
-  print('loadcuda')
-  if params.gpu >= 0 then
-    cnn:cuda()
-  end
-  
-  -- Lists all the files from the style directory
-  print('get images')
-  local style_size = math.ceil(params.style_scale * params.image_size)
-  local style_images = {}
-  for file in io.popen('find "'..params.style_dir..'" -maxdepth 1 -type f'):lines() do
-    print("found file "..file)
-    table.insert(style_images, file)
-  end
-  
-  
-  local content_layers = params.content_layers:split(",")
-  local style_layers = params.style_layers:split(",")
+   if params.backend == 'cudnn' then
+      require 'cudnn'
+      cudnn.SpatialConvolution.accGradParameters = nn.SpatialConvolutionMM.accGradParameters -- ie: nop
+   end
+   
+   print('loadcaffe')
+   local cnn = loadcaffe_wrap.load(params.proto_file, params.model_file, params.backend):float()
+   print('loadcuda')
+   if params.gpu >= 0 then
+      cnn:cuda()
+   end
+   
+   -- Lists all the files from the style directory
+   print('get images')
+   local style_size = math.ceil(params.style_scale * params.image_size)
+   local style_images = {}
+   for file in io.popen('find "'..params.style_dir..'" -maxdepth 1 -type f'):lines() do
+      print("found file "..file)
+      table.insert(style_images, file)
+   end
+   
+   
+   local content_layers = params.content_layers:split(",")
+   local style_layers = params.style_layers:split(",")
 
-  -- Set up the network, inserting style descriptor modules
-  local style_descrs = {}
-  local next_style_idx = 1
-  local net = nn.Sequential()
-  for i = 1, #cnn do
-    if next_style_idx <= #style_layers then
-      local layer = cnn:get(i)
-      local name = layer.name
-      local layer_type = torch.type(layer)
-      local is_pooling = (layer_type == 'cudnn.SpatialMaxPooling' or layer_type == 'nn.SpatialMaxPooling')
-      if is_pooling and params.pooling == 'avg' then
-        assert(layer.padW == 0 and layer.padH == 0)
-        local kW, kH = layer.kW, layer.kH
-        local dW, dH = layer.dW, layer.dH
-        local avg_pool_layer = nn.SpatialAveragePooling(kW, kH, dW, dH):float()
-        if params.gpu >= 0 then avg_pool_layer:cuda() end
-        local msg = 'Replacing max pooling at layer %d with average pooling'
-        print(string.format(msg, i))
-        net:add(avg_pool_layer)
-      else
-        net:add(layer)
+   -- Set up the network, inserting style descriptor modules
+   local style_descrs = {}
+   local next_style_idx = 1
+   local net = nn.Sequential()
+   for i = 1, #cnn do
+      if next_style_idx <= #style_layers then
+	 local layer = cnn:get(i)
+	 local name = layer.name
+	 local layer_type = torch.type(layer)
+	 local is_pooling = (layer_type == 'cudnn.SpatialMaxPooling' or layer_type == 'nn.SpatialMaxPooling')
+	 if is_pooling and params.pooling == 'avg' then
+	    assert(layer.padW == 0 and layer.padH == 0)
+	    local kW, kH = layer.kW, layer.kH
+	    local dW, dH = layer.dW, layer.dH
+	    local avg_pool_layer = nn.SpatialAveragePooling(kW, kH, dW, dH):float()
+	    if params.gpu >= 0 then avg_pool_layer:cuda() end
+	    local msg = 'Replacing max pooling at layer %d with average pooling'
+	    print(string.format(msg, i))
+	    net:add(avg_pool_layer)
+	 else
+	    net:add(layer)
+	 end
+	 if name == style_layers[next_style_idx] then
+	    print("Setting up style layer  ", i, ":", layer.name)
+	    local style_module = nn.StyleDescr(params.style_weight):float()
+	    if params.gpu >= 0 then
+	       style_module:cuda()
+	    end
+	    net:add(style_module)
+	    table.insert(style_descrs, style_module)
+	    next_style_idx = next_style_idx + 1
+	 end
       end
-      if name == style_layers[next_style_idx] then
-        print("Setting up style layer  ", i, ":", layer.name)
-        local style_module = nn.StyleDescr(params.style_weight):float()
-        if params.gpu >= 0 then
-          style_module:cuda()
-        end
-        net:add(style_module)
-        table.insert(style_descrs, style_module)
-        next_style_idx = next_style_idx + 1
-      end
-    end
-  end
+   end
 
-  -- We don't need the base CNN anymore, so clean it up to save memory.
-  cnn = nil
-  for i=1,#net.modules do
-    local module = net.modules[i]
-    if torch.type(module) == 'nn.SpatialConvolutionMM' then
-        -- remote these, not used, but uses gpu memory
-        module.gradWeight = nil
-        module.gradBias = nil
-    end
-  end
-  collectgarbage()
-  
-  
-  -- Run each image to the network and save their style features
-  for i=1, #style_images do
-    local img = image.load(style_image[i], 3)
-    img = image.scale(img, style_size, 'bilinear')
-    local img_caffe = preprocess(img):float()
-    if params.gpu >= 0 then
-      img = img:cuda()
-    end
-    net:forward(img)
-    for _, mod in ipairs(style_descrs) do
-      -- TODO SAVE mod.G
-    end
-  end
-  
-  -- Unsupervised learning of the features
-  -- TODO PCA on the style features
-  
-  -- Save final features
+   -- We don't need the base CNN anymore, so clean it up to save memory.
+   cnn = nil
+   for i=1,#net.modules do
+      local module = net.modules[i]
+      if torch.type(module) == 'nn.SpatialConvolutionMM' then
+	 -- remote these, not used, but uses gpu memory
+	 module.gradWeight = nil
+	 module.gradBias = nil
+      end
+   end
+   collectgarbage()
+   
+   local outputs = {}
+   for i=1, #style_descrs do
+      table.insert(outputs, {})
+   end
+
+   net = nil
+   collectgarbage()
+   
+   -- Run each image to the network and save their style features
+   for i=1, #style_images do
+      print('processing image '..i..': '..style_image[i])
+      local img = image.load(style_image[i], 3)
+      if img then
+	 img = image.scale(img, style_size, 'bilinear')
+	 local img_caffe = preprocess(img):float()
+	 if params.gpu >= 0 then
+	    img = img:cuda()
+	 end
+	 net:forward(img)
+	 for j, mod in ipairs(style_descrs) do
+	    table.insert(outputs[j], mod.G:clone())
+	 end
+      end
+   end
+   
+   -- Unsupervised learning of the features
+   local style_outputs = {}
+   for i=1, #style_descrs do
+      local st = torch.Tensor(#outputs[i], outputs[i][1]:nElement())
+      for j=1, #outputs[i] do
+	 for k=1, #outputs[i][j] do
+	    st[j][k] = #outputs[i][j][k]
+	 end
+      end
+      local ce, cv = pcacov(st)
+      local ref = torch.mean(torch.mm(st, cv), 1)
+      table.insert(style_outputs, {cv, ref, ce})
+   end
+   
+   -- Save final features
+   torch.save(style_outputs, params.output_file)
 end
 
 
 function build_filename(output_image, iteration)
-  local ext = paths.extname(output_image)
-  local basename = paths.basename(output_image, ext)
-  return string.format('%s_%d.%s', basename, iteration, ext)
+   local ext = paths.extname(output_image)
+   local basename = paths.basename(output_image, ext)
+   return string.format('%s_%d.%s', basename, iteration, ext)
 end
 
 
@@ -149,37 +169,51 @@ end
 -- We need to rescale from [0, 1] to [0, 255], convert from RGB to BGR,
 -- and subtract the mean pixel.
 function preprocess(img)
-  local mean_pixel = torch.DoubleTensor({103.939, 116.779, 123.68})
-  local perm = torch.LongTensor{3, 2, 1}
-  img = img:index(1, perm):mul(256.0)
-  mean_pixel = mean_pixel:view(3, 1, 1):expandAs(img)
-  img:add(-1, mean_pixel)
-  return img
+   local mean_pixel = torch.DoubleTensor({103.939, 116.779, 123.68})
+   local perm = torch.LongTensor{3, 2, 1}
+   img = img:index(1, perm):mul(256.0)
+   mean_pixel = mean_pixel:view(3, 1, 1):expandAs(img)
+   img:add(-1, mean_pixel)
+   return img
 end
 
 
 -- Undo the above preprocessing.
 function deprocess(img)
-  local mean_pixel = torch.DoubleTensor({103.939, 116.779, 123.68})
-  mean_pixel = mean_pixel:view(3, 1, 1):expandAs(img)
-  img = img + mean_pixel
-  local perm = torch.LongTensor{3, 2, 1}
-  img = img:index(1, perm):div(256.0)
-  return img
+   local mean_pixel = torch.DoubleTensor({103.939, 116.779, 123.68})
+   mean_pixel = mean_pixel:view(3, 1, 1):expandAs(img)
+   img = img + mean_pixel
+   local perm = torch.LongTensor{3, 2, 1}
+   img = img:index(1, perm):div(256.0)
+   return img
+end
+
+
+-- from package unsup by koraykv
+-- PCA using covariance matrix
+-- x is supposed to be MxN matrix, where M samples(trials) and each sample(trial) is N dim
+-- returns the eigen values and vectors of the covariance matrix in increasing order
+function pcacov(x)
+   local mean = torch.mean(x,1)
+   local xm = x - torch.ger(torch.ones(x:size(1)),mean:squeeze())
+   local c = torch.mm(xm:t(),xm)
+   c:div(x:size(1)-1)
+   local ce,cv = torch.symeig(c,'V')
+   return ce,cv
 end
 
 
 -- Returns a network that computes the CxC Gram matrix from inputs
 -- of size C x H x W
 function GramMatrix()
-  local net = nn.Sequential()
-  net:add(nn.View(-1):setNumInputDims(2))
-  local concat = nn.ConcatTable()
-  concat:add(nn.Identity())
-  concat:add(nn.Identity())
-  net:add(concat)
-  net:add(nn.MM(false, true))
-  return net
+   local net = nn.Sequential()
+   net:add(nn.View(-1):setNumInputDims(2))
+   local concat = nn.ConcatTable()
+   concat:add(nn.Identity())
+   concat:add(nn.Identity())
+   net:add(concat)
+   net:add(nn.MM(false, true))
+   return net
 end
 
 
@@ -187,23 +221,23 @@ end
 local StyleDescr, parent = torch.class('nn.StyleDescr', 'nn.Module')
 
 function StyleDescr:__init(strength)
-  parent.__init(self)
-  self.strength = strength
-  
-  self.gram = GramMatrix()
-  self.G = nil
+   parent.__init(self)
+   self.strength = strength
+   
+   self.gram = GramMatrix()
+   self.G = nil
 end
 
 function StyleDescr:updateOutput(input)
-  self.G = self.gram:forward(input)
-  self.G:div(input:nElement())
-  self.output = input
-  return self.output
+   self.G = self.gram:forward(input)
+   self.G:div(input:nElement())
+   self.output = input
+   return self.output
 end
 
 -- Useless ?
 function StyleDescr:updateGradInput(input, gradOutput)
-  return gradOutput
+   return gradOutput
 end
 
 
